9c4cebdfdd5750a4f29061ffdee81f2faf0ec2ff
jaso69 <joseonieva@gmail.com>
2025-09-24 21:04:32 +0200

    astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,12 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   integrations: [clerk(), tailwindcss()],
-    adapter: vercel({
-    // Configuración opcional para Vercel
-    edgeMiddleware: true,
+  adapter: vercel({
+    // Configuración importante
+    edgeMiddleware: false, // Desactiva edge middleware
+    functionPerRoute: false, // Una función única
+    // Especifica que es para Node.js
+    platform: 'node'
   }),
   output: 'server',
 })
